5339cbdcc8457b3706a7bfd73ed0803e6406dda2
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-07-16 23:46:08 +0600

    team added

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -2,42 +2,12 @@ import React from "react";
 
 const ContactUs = () => {
   return (
-    <div className="my-8">
-      {/* <h1 className="text-2xl font-bold my-8">Contact </h1> */}
-      {/* <div class="card mx-auto w-96 bg-base-100 shadow-2xl">
-        <div class="card-body">
-          <h2 class="card-title mb-8 mx-auto font-bold text-2xl">
-            Contact Us with Email
-          </h2>
-          <input
-            type="text"
-            name="name"
-            placeholder="Type your name"
-            class="input input-bordered input-error w-full max-w-xs"
-          />
-          <input
-            type="text"
-            name="email"
-            placeholder="Type your email"
-            class="input input-bordered input-error w-full max-w-xs"
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Type password"
-            class="input input-bordered input-error w-full max-w-xs"
-          />
-          <div class="card-actions justify-center">
-            <button class="btn btn-btn btn-md bg-[#FF8A65] border-none ">
-              Send Email
-            </button>
-          </div>
-        </div>
-      </div> */}
-
+    <div className="mt-8">
       <div
         class="hero min-h-screen"
-        style={{ backgroundImage: ` url(https://placeimg.com/1000/800/arch)` }}
+        style={{
+          backgroundImage: ` url(https://thumbs.dreamstime.com/b/contact-us-customer-support-hotline-people-connect-call-custo-130157516.jpg)`,
+        }}
       >
         <div class="hero-overlay bg-opacity-60"></div>
         <div class="hero-content text-center text-neutral-content">
@@ -65,6 +35,11 @@ const ContactUs = () => {
                   placeholder="Type password"
                   class="input input-bordered input-error w-full max-w-xs"
                 />
+                <textarea
+                  name="message"
+                  class="textarea textarea-error"
+                  placeholder="Type your message"
+                ></textarea>
                 <div class="card-actions justify-center">
                   <button class="btn btn-btn btn-md bg-[#FF8A65] border-none ">
                     Send Email
--- src/Components/Team.js
+++ src/Components/Team.js
@@ -1,7 +1,304 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import md from "../Images/Md.png";
 
 const Team = () => {
-  return <div>welcome to team</div>;
+  return (
+    <div className="mt-8 mb-8">
+      <div className="propretor">
+        <div class="avatar ">
+          <div class="w-28  rounded">
+            <img src={md} />
+          </div>
+        </div>
+        <h2 className=" font-bold">Mr. Abu Taleb</h2>
+        <span className="">Propretor</span>
+        <div className="media flex justify-center items-center">
+          <a href="">
+            <img
+              className=" w-8 m-2"
+              src="https://img.icons8.com/ios-filled/344/facebook-new.png"
+              alt=""
+            />
+          </a>
+          <a href="">
+            <img
+              className=" w-8 m-2 "
+              src="https://img.icons8.com/ios-filled/344/twitter-circled--v1.png"
+              alt=""
+            />
+          </a>
+          <a href="">
+            <img
+              className=" w-8 m-2"
+              src="https://img.icons8.com/ios-filled/344/linkedin-circled.png"
+              alt=""
+            />
+          </a>
+        </div>
+        <hr />
+      </div>
+
+      <div className="team mt-8 grid gap-8 grid-cols-2 lg:grid-cols-4 mx-12 lg:mx-72">
+        <div className="team-members">
+          <div class="avatar">
+            <div class="w-28 rounded-full ring  ring-offset-base-100 ring-offset-2 ring  ring-offset-base-100 ring-offset-2">
+              <img src="https://placeimg.com/192/192/people" />
+            </div>
+          </div>
+          <h2 className=" font-bold">Md Abdul Karim</h2>
+          <span className="">CEO</span>
+          <div className="media flex justify-center items-center">
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/facebook-new.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2 "
+                src="https://img.icons8.com/ios-filled/344/twitter-circled--v1.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/linkedin-circled.png"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+        <div className="team-members">
+          <div class="avatar">
+            <div class="w-28 rounded-full ring  ring-offset-base-100 ring-offset-2">
+              <img src="https://placeimg.com/192/192/people" />
+            </div>
+          </div>
+          <h2 className=" font-bold">Md Abdul Karim</h2>
+          <span className="">CEO</span>
+          <div className="media flex justify-center items-center">
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/facebook-new.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2 "
+                src="https://img.icons8.com/ios-filled/344/twitter-circled--v1.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/linkedin-circled.png"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+        <div className="team-members">
+          <div class="avatar">
+            <div class="w-28 rounded-full ring  ring-offset-base-100 ring-offset-2">
+              <img src="https://placeimg.com/192/192/people" />
+            </div>
+          </div>
+          <h2 className=" font-bold">Md Abdul Karim</h2>
+          <span className="">CEO</span>
+          <div className="media flex justify-center items-center">
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/facebook-new.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2 "
+                src="https://img.icons8.com/ios-filled/344/twitter-circled--v1.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/linkedin-circled.png"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+        <div className="team-members">
+          <div class="avatar">
+            <div class="w-28 rounded-full ring  ring-offset-base-100 ring-offset-2">
+              <img src="https://placeimg.com/192/192/people" />
+            </div>
+          </div>
+          <h2 className=" font-bold">Md Abdul Karim</h2>
+          <span className="">CEO</span>
+          <div className="media flex justify-center items-center">
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/facebook-new.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2 "
+                src="https://img.icons8.com/ios-filled/344/twitter-circled--v1.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/linkedin-circled.png"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+        <div className="team-members">
+          <div class="avatar">
+            <div class="w-28 rounded-full ring  ring-offset-base-100 ring-offset-2">
+              <img src="https://placeimg.com/192/192/people" />
+            </div>
+          </div>
+          <h2 className=" font-bold">Md Abdul Karim</h2>
+          <span className="">CEO</span>
+          <div className="media flex justify-center items-center">
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/facebook-new.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2 "
+                src="https://img.icons8.com/ios-filled/344/twitter-circled--v1.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/linkedin-circled.png"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+        <div className="team-members">
+          <div class="avatar">
+            <div class="w-28 rounded-full ring  ring-offset-base-100 ring-offset-2">
+              <img src="https://placeimg.com/192/192/people" />
+            </div>
+          </div>
+          <h2 className=" font-bold">Md Abdul Karim</h2>
+          <span className="">CEO</span>
+          <div className="media flex justify-center items-center">
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/facebook-new.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2 "
+                src="https://img.icons8.com/ios-filled/344/twitter-circled--v1.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/linkedin-circled.png"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+        <div className="team-members">
+          <div class="avatar">
+            <div class="w-28 rounded-full ring  ring-offset-base-100 ring-offset-2">
+              <img src="https://placeimg.com/192/192/people" />
+            </div>
+          </div>
+          <h2 className=" font-bold">Md Abdul Karim</h2>
+          <span className="">CEO</span>
+          <div className="media flex justify-center items-center">
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/facebook-new.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2 "
+                src="https://img.icons8.com/ios-filled/344/twitter-circled--v1.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/linkedin-circled.png"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+        <div className="team-members">
+          <div class="avatar">
+            <div class="w-28 rounded-full ring  ring-offset-base-100 ring-offset-2">
+              <img src="https://placeimg.com/192/192/people" />
+            </div>
+          </div>
+          <h2 className=" font-bold">Md Abdul Karim</h2>
+          <span className="">CEO</span>
+          <div className="media flex justify-center items-center">
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/facebook-new.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2 "
+                src="https://img.icons8.com/ios-filled/344/twitter-circled--v1.png"
+                alt=""
+              />
+            </a>
+            <a href="">
+              <img
+                className=" w-8 m-2"
+                src="https://img.icons8.com/ios-filled/344/linkedin-circled.png"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Team;
